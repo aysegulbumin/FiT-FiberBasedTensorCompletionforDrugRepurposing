6dc8b0d68c06704791e756d5c4a205db2139ef4e
aysegulbumin <aysegul.bumin@ufl.edu>
2022-02-16 16:36:32 -0500

    Pearson correlation is added

---
+++ CalculateTestTensorPearson.py
@@ -0,0 +1,250 @@
+import pickle
+import sys
+import random
+import pandas as pd
+import numpy as np
+import io
+import multiprocessing as mp
+import time
+import torch
+
+from scipy import stats
+
+imputed_file =str(sys.argv[1])
+test_id =int(sys.argv[2])
+
+print("Loading Set from pickle file.")
+
+file_to_read = open("/blue/tkahveci/aysegul.bumin/Data/Set.pkl", "rb")
+
+Set = pickle.load(file_to_read)
+
+#print(Loaded_Set.keys())
+
+file_to_read.close()
+print("Loading run1...")
+
+file_name = "/blue/tkahveci/aysegul.bumin/Data/run1.pkl"
+
+open_file = open(file_name, "rb")
+run1 = pickle.load(open_file)
+open_file.close()
+print("Loading run2...")
+
+file_name = "/blue/tkahveci/aysegul.bumin/Data/run2.pkl"
+
+open_file = open(file_name, "rb")
+run2 = pickle.load(open_file)
+open_file.close()
+print("Loading run3...")
+
+file_name = "/blue/tkahveci/aysegul.bumin/Data/run3.pkl"
+
+open_file = open(file_name, "rb")
+run3 = pickle.load(open_file)
+open_file.close()
+print("Loading run4...")
+
+file_name = "/blue/tkahveci/aysegul.bumin/Data/run4.pkl"
+
+open_file = open(file_name, "rb")
+run4 = pickle.load(open_file)
+open_file.close()
+print("Loading run5...")
+
+file_name = "/blue/tkahveci/aysegul.bumin/Data/run5.pkl"
+
+open_file = open(file_name, "rb")
+run5 = pickle.load(open_file)
+open_file.close()
+
+for key in Set.keys():
+    Set[key]= torch.FloatTensor(Set[key])
+
+# O is the missing values
+# 1 is the test values
+# 2 is the validation 
+# 3,4,5 are the training samples
+
+test_list = []
+train_list = []
+
+test_list = [(i,j) for i,j  in list(Set.keys()) if run1[i,j]== test_id]
+
+train_list = [(i,j) for i,j in list(Set.keys()) if (run1[i,j]!=test_id and run1[i,j] != 0) ] #if it is not missing value (zero) and if it is not test (one) and if it is not validation (val) then it is training 
+
+print("Training Samples: ", len(train_list))
+print("Test Samples: ", len(test_list))
+
+
+#ImputedTensorSeed0Level3.pkl
+
+file_to_read = open(imputed_file, "rb")
+
+ImputedTensorCompletion = pickle.load(file_to_read)
+
+file_to_read.close()
+
+
+def PredictwithMedian(i1,j1,k,Set):
+    rows = []
+    cols = []
+    #for i1, j1 in test_samples:
+    # fixed i- row 
+    for (i_, j_) in list(Set.keys()):
+      if j_ !=j1 and i_==i1:
+        rows.append( float(Set[(i1,j_)][k]))
+    for (i_,j_) in list(Set.keys()):
+      if i_!=i1 and j_==j1:
+        cols.append(float(Set[(i_,j1)][k]))
+    rows= np.array(rows)
+    rows_median = np.median(rows)
+    cols= np.array(cols)
+    cols_median = np.median(cols)
+
+    predicted= (rows_median+ cols_median)/2.0
+
+    return predicted
+
+import time
+#0.950 with test set size 1605 training set 24K
+normalized_loss_list = []
+mse_loss_list = []
+rmse_list=[]
+GeneError = [0 for i in range(978)]
+summed_error = 0
+summed_norm = 0
+CellDictPredicted = {}
+CellDictActual = {}
+for i,j in test_list:
+    for k in range(978):
+      predicted = PredictwithMedian(i,j,k,Set)
+      actual = Set[(i,j)][k]
+
+      summed_norm += (actual)**2
+      error = (predicted-actual)**2
+      summed_error += error
+      GeneError[k] = np.sqrt((predicted-actual)**2)/np.sqrt((actual)**2)
+      if i not in list(CellDictPredicted.keys()):
+          CellDictPredicted[i] = []
+          CellDictActual[i] = []
+      else:
+          CellDictPredicted[i].append(predicted)
+          CellDictActual[i].append(actual)
+
+rmse = np.sqrt(summed_error)/ np.sqrt(summed_norm)
+print("Per Gene Error in Two Way Algorithm")
+print(GeneError)
+print("Test error:", rmse)
+
+#WEIGHTED SPEARMAN CORRELATION 
+
+import sys
+import numpy as np
+from scipy.stats import rankdata
+from scipy.stats import norm
+
+# Obtain weighted rank correlation between x and y.
+
+# specify weights
+def weights(x, n, e=0.01):
+    w = 2.0*norm.pdf(x-0.5, 0, n*e)
+    return w
+
+# weighted Spearman Correlation
+def weighted_spearman(x, y):
+    n = len(x)
+    x_ranked = rankdata(x)
+    y_ranked = rankdata(y)
+    s = 0.0
+    
+    for i in range(0,n):
+        # get the weights
+        w = weights(x_ranked[i], n)
+        #w = 1/n #for uniform weights (Shieh et al. 2000)
+        d = w * (x_ranked[i] - ((n+1)/2)) * (y_ranked[i] - ((n+1)/2))
+        s = s + d
+    
+    return s
+    
+# weighted rank correlation
+def weighted_rank_cor(x, y, positive=True):
+    if(positive):
+        x = np.negative(x)
+        y = np.negative(y)
+    
+    return weighted_spearman(x, y)/weighted_spearman(x, x)
+
+from scipy import stats
+
+print("TWO WAY")
+correlations = []
+p_vals =  []
+#cor_weighted = []
+#correlations_stat = []
+for key, value in CellDictPredicted.items():
+    predicted = value
+    actual =  CellDictActual[key]
+    coeff, p_val = stats.pearsonr(predicted, actual)
+    #spearman,_ = stats.spearmanr(predicted,actual)
+    print("Cell Line: ",key)
+    print("Pearson coeff : ",coeff)
+    print("P-val : ", p_val)
+    correlations.append(coeff)
+    p_vals.append(p_val)
+
+
+print("TWO WAY")
+print("Correlations: ",correlations)
+print("Mean of Correlations: ", np.mean(correlations))
+print("P-values:", p_vals)
+
+
+
+print("Calculating the Test Error :")
+
+rmse = 0
+norm_square = 0
+error = 0
+
+GeneError = [0 for i in range(978)]
+CellDictPredicted = {}
+CellDictActual = {}
+for i,j in test_list:
+    for k in range(978):
+        predicted = ImputedTensorCompletion[(i,j)][k]
+        actual = Set[(i,j)][k]
+        error += (predicted-actual)**2
+        norm_square += (actual)**2
+        GeneError[k] = np.sqrt((predicted-actual)**2)/np.sqrt((actual)**2)
+        if i not in list(CellDictPredicted.keys()):
+            CellDictPredicted[i] = []
+            CellDictActual[i] = []
+        else:
+            CellDictPredicted[i].append(predicted)
+            CellDictActual[i].append(actual)
+
+
+rmse = np.sqrt(error)/np.sqrt(norm_square)
+print("Per Gene Error in Tensor Completion with " + imputed_file)
+print(GeneError)
+print(rmse)
+
+print("TENSOR COMPLETION")
+correlations = []
+p_vals = []
+for key, value in CellDictPredicted.items():
+    predicted = value
+    actual =  CellDictActual[key]
+    coeff, p_val = stats.pearsonr(predicted, actual)
+    #spearman,_ = stats.spearmanr(predicted,actual)
+    print("Cell Line: ",key)
+    print("Pearson coeff : ",coeff)
+    print("P-val : ", p_val)
+    correlations.append(coeff)
+    p_vals.append(p_val)
+
+print("Tensor Completion")
+print("Correlations: ",correlations)
+print("Mean of Correlations: ", np.mean(correlations))
+print("P-values:", p_vals)
